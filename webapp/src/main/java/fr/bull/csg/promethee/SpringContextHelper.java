ecdef64942a06189ef11a8d5136aa7d3b3fcb63a
martiben <no@email.com>
2013-03-27 14:41:30 +0100

    Test OK avec aspects

---
+++ webapp/src/main/java/fr/bull/csg/promethee/SpringContextHelper.java
@@ -0,0 +1,22 @@
+package fr.bull.csg.promethee;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class SpringContextHelper
+{
+
+   private ApplicationContext context;
+
+   public SpringContextHelper(ServletContext servletContext)
+   {
+      context = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
+   }
+
+   public Object getBean(final String beanRef)
+   {
+      return context.getBean(beanRef);
+   }
+}
